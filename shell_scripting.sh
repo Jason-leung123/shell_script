92aca45db2a95994c1c3c92e6481f5689d4688e3
Jason Leung <jason.leung19062000@gmail.com>
2018-11-16 17:04:01 +0000

    first comit

---
+++ shell_scripting.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#update date daily at 6:00 AM
+#Author: Solomon Amos
+
+ DATE=`date "+%d-%m-%Y %a %p %l:%M %Z"`
+ Weather=$(curl wttr.in/?0?q?T | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$0}')
+ time=$(date +%d-%m-%Y).png
+
+ echo -e "* $DATE   : $Weather" | tee -a ~/shell_scripting/README.md
+
+ date +%d-%m-%Y >> ~/shell_scripting/weather.md
+
+ curl -L 'wttr.in/York=.png' -o ~/shell_scripting/img/$time
+ 
+  echo -e "\n![](img/$time)\n" >> ~/shell_scripting/weather.md
+
+ cd ~/shell_scripting
+ git init
+ git add .
+ git commit -m "first comit"
+ git remote add origin https://github.com/Jason-leung123/shell_script.git
+ git push -f origin master
